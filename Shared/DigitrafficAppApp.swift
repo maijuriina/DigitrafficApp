5d429a1a4455932a318146c793a460751ca59bdd
Maijuriina Mustonen <42972494+maijuriina@users.noreply.github.com>
2021-03-08 10:57:39 +0200

    Initial Commit

---
+++ Shared/DigitrafficAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigitrafficAppApp.swift
+//  Shared
+//
+//  Created by Maijuriina Mustonen on 08/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct DigitrafficAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
