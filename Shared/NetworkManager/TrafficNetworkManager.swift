ed181a5bc7f0e727b7f2d064b2c9bbd5816eac87
Maijuriina Mustonen <42972494+maijuriina@users.noreply.github.com>
2021-03-22 09:41:59 +0200

    JSON decoder and urlrequest created for NetworkManager

--- Shared/NetworkManager/TrafficNetworkManager.swift
+++ Shared/NetworkManager/TrafficNetworkManager.swift
@@ -12,6 +12,24 @@ class TrafficNetworkManager : NetworkManagerProtocol {
         // use index of list object as roadName...
         let API_URL = "https://tie.digitraffic.fi/api/v1/data/camera-data"
     
-    
+        guard let url = URL(string: API_URL) else { // creates the url
+            fatalError()
+        }
+        print(url)
+        
+        // start urlsession with request
+        let urlRequest = URLRequest(url: url)
+        URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+            guard let data = data else { return }
+
+            // if not nil, decode with TrafficModel
+            do {
+                let currentTraffic = try JSONDecoder().decode(TrafficModel.self, from: data)
+                print(currentTraffic)
+                completion(currentTraffic)
+            } catch {
+                print(error)
+            }
+        }.resume()
     }
 }
