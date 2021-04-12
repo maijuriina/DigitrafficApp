6a16a3f2c46ba828133b1649f6508a3981723146
Maijuriina Mustonen <42972494+maijuriina@users.noreply.github.com>
2021-04-12 10:41:05 +0300

    Added appicons and launch screen

--- Shared/Views/ImageView.swift
+++ Shared/Views/ImageView.swift
@@ -8,11 +8,13 @@
 import SwiftUI
 
 struct ImageView: View {
-    
+    @EnvironmentObject var networkManager: TrafficNetworkManager
     let imageUrl: URL
     
     var body: some View {
         AsyncImage(url: imageUrl, placeholder: Text("Loading image.."))
             .aspectRatio(contentMode: .fit)
+            .padding(2)
+        Spacer()
     }
 }
